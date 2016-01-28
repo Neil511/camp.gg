f40cb9c00dfbb4b27deec100044bbbe873aa0ea4
Neil Lohana <Neil_lohana@hotmail.com>
2016-01-27 20:48:25 -0500

    Wrote JS function to get summonerObj by summonerName

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,23 @@
+$(document).ready( function (){
+	console.log("Loaded main.js!");
+});
+
+var key = "api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
+var url = "";
+
 function get(url){
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", url, false);
-	xmlHttp.send(null);
-	return xmlHttp.responseText;
+	var xmr = new XMLHttpRequest();
+	xmr.open("GET", url, false);
+	xmr.send(null);
+	return xmr.response;
+};
+
+function getSummonerByName(summonerName, region){
+	return get("https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + key);
 };
 
 $('.search').click(function () {
-	console.log("Hello!");
-})
+	console.log($('#searchBar').val());
+	getSummonerByName($('#searchBar').val(), "na");
+});
+
