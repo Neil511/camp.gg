64d5d52cb5f2b66f3646303dada108a6e2ef6e2d
Neil Lohana <Neil_lohana@hotmail.com>
2016-01-27 21:04:25 -0500

    Fixed get request to parse JSON into obj from string

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,18 +2,23 @@ $(document).ready( function (){
 	console.log("Loaded main.js!");
 });
 
-var key = "api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
-var url = "";
+var key = "?api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
+var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Nubsee?api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
 
 function get(url){
 	var xmr = new XMLHttpRequest();
 	xmr.open("GET", url, false);
 	xmr.send(null);
-	return xmr.response;
+	console.log("GOT: " + xmr.response);
+	return JSON.parse(xmr.response);
 };
 
 function getSummonerByName(summonerName, region){
-	return get("https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + key);
+	var response = get("https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + key);
+	//console.log(response.id);
+	console.log(response);
+	console.log(response.id);
+	console.log(response['id']);
 };
 
 $('.search').click(function () {
