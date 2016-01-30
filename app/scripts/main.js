dda9cc318f398c9def09058b449ca1c61ad1e097
Neil Lohana <Neil_lohana@hotmail.com>
2016-01-30 13:50:36 -0500

    Added function to fetch ranked stats for Solo5x5 season5

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,8 @@ $(document).ready( function (){
 	console.log("Loaded main.js!");
 });
 
-var key = "?api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
-var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Nubsee?api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
+var key = "api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
+var baseUrl = "https://na.api.pvp.net/api/lol/";
 
 function get(url){
 	var xmr = new XMLHttpRequest();
@@ -13,14 +13,32 @@ function get(url){
 	return JSON.parse(xmr.response);
 };
 
-function getSummonerByName(summonerName, region){
-	var response = get("https://na.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + key);
+// Uses a summoner name to fetch summoner object and get summoner ID
+function getSummonerID(summonerName, region){
+	var response = get(baseUrl + region + "/v1.4/summoner/by-name/" + summonerName + "?" + key);
 	var id = response[summonerName.toLowerCase()].id; 
 	console.log(id);
+	return id;
 };
+
+
+function getRankedStats(summonerID, region){
+	var response = get(baseUrl + region + "/v1.3/stats/by-summoner/" + summonerID + "/summary?season=SEASON2016&" + key);
+	var allStats = response['playerStatSummaries'];
+	console.log(allStats);
+	var i = 0;
+	for(var x = 0; x < 10; x++){
+		i++;
+		console.log(i);
+		if(allStats[x].playerStatSummaryType === 'RankedSolo5x5'){
+			console.log("Found it!");
+		}
+	}
+}
+
 
 $('.search').click(function () {
 	console.log($('#searchBar').val());
-	getSummonerByName($('#searchBar').val(), "na");
+	getRankedStats(getSummonerID($('#searchBar').val(), "na"), "na");
 });
 
