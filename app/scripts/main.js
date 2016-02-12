2a319c8463547273d8e2f6b6ed0c3859802055c6
Neil Lohana <Neil_lohana@hotmail.com>
2016-02-11 23:30:06 -0500

    Added control flow for the rest of the methods

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,6 @@
 $(document).ready( function (){
     console.log("Loaded main.js!");
 
-
     var key = "api_key=23a00ef6-0a77-4aa2-b278-8d402f4e720c";
     var baseUrl = "https://na.api.pvp.net/api/lol/";
 
@@ -14,16 +13,17 @@ $(document).ready( function (){
     }
 
     // Uses a summoner name to fetch summoner object and get summoner ID
-    function getSummonerID(summonerName, region){
+    function getSummonerID(summonerName, region) {
         var response = get(baseUrl + region + "/v1.4/summoner/by-name/" + summonerName + "?" + key);
         summonerName = (summonerName.toLowerCase()).split(' ').join('');
         var id = response[summonerName].id;
         return id;
     }
 
-    function getRankedStats(summonerID, region){
+    // Get's ranked stats by summoner id
+    function getRankedStats(summonerID, region) {
         var response = get(baseUrl + region + "/v1.3/stats/by-summoner/" + summonerID + "/summary?season=SEASON2016&" + key);
-        var allStats = response['playerStatSummaries'];
+        var allStats = response.playerStatSummaries;
         var rankedStats;
         for(var type in allStats){
             if(allStats[type].playerStatSummaryType === 'RankedSolo5x5'){
@@ -31,6 +31,17 @@ $(document).ready( function (){
             }
         }
         return rankedStats;
+        // function calls to calculate win rate and other stuff
+    }
+
+    // function that fetches all the users in the player's current game
+    function getCurrentGame(summonerName, region) {
+
+    }
+
+    // returns a person's ranked win rate as a decimal value
+    function getWinRate(rankedData) {
+
     }
 
     $('.search').click(function () {
